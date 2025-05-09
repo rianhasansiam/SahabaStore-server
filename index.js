4ec0ffe0590eae31b9e4a4856f9285a95ef7942d
rianhasansiam <rianhasan1971@gmail.com>
2025-05-09 18:27:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2cje9ya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ Successfully connected to MongoDB");
+
+    app.get("/", (req, res) => {
+      res.send("Server is running!");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    app.listen(port, () => {
+      console.log(`🚀 Server running on http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("❌ Error connecting to MongoDB:", err);
+  }
+}
+
+run().catch(console.dir);
