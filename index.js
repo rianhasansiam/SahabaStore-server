6a3d8e273719f7added55aee4afc13836f2c7374
rianhasansiam <rianhasan1971@gmail.com>
2025-05-11 04:16:13 +0600

    users related api done

--- index.js
+++ index.js
@@ -26,22 +26,103 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("✅ Successfully connected to MongoDB");
 
-    app.get("/", (req, res) => {
-      res.send("Server is running!");
+
+// Collections
+    const datafile = client.db("sahabastore");
+    const userInfoCollection = datafile.collection("users");
+    const categoriesCollection = datafile.collection("categories");
+    const productsCollection = datafile.collection("products");
+   
+
+
+
+
+
+      app.get("/", (req, res) => {
+      res.send("Hello..! This is Rian Hasan....");
+    });
+
+
+//user data post
+
+app.post("/userData", async (req,res)=>{
+
+try{
+
+let userData = req.body;
+
+
+// Check if the user already exists in the database (you can use email or another unique field as a filter)
+        const existingUser = await userInfoCollection.findOne({
+          email: userData.email,
+        });
+    
+        if (existingUser) {
+          // If the user already exists, send a response and avoid insertion
+          // Console.log("kire abar id khulos")
+          return res.send({ message: "User already exists" });
+        } else {
+          // Add a default role to the user data
+          userData.userRole = "Customer";
+    
+          // If the user doesn't exist, insert the new user data with the role
+          const result = await userInfoCollection.insertOne(userData);
+          
+          // Send success response
+          res.send(result);
+        }
+
+
+} catch{
+
+ console.error("Error inserting user data:", error);
+        res.status(500).send("Internal Server Error");
+
+}
+
+
+
+
+})
+
+
+
+
+//fetch all user
+  app.get("/allusers", async (req, res) => {
+      try {
+        const result = await userInfoCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
     });
 
 
 
 
+ // Route to delete a user
+    app.delete("/delete-user/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+      
 
+        const query = { _id: new ObjectId(id) };
 
+        const result = await userInfoCollection.deleteOne(query);
 
-
-
-
-
-
-
+        if (result.deletedCount === 1) {
+          res
+            .status(200)
+            .send({ message: "Application canceled successfully" });
+        } else {
+          res.status(404).send({ message: "Application not found" });
+        }
+      } catch (error) {
+        console.error("Error canceling application:", error);
+        res.status(500).send({ message: "Error canceling application" });
+      }
+    });
 
 
 
